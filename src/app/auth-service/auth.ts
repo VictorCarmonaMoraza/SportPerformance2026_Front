1b323496ecec523e24aa7f7c8f4b5f8d4a29911c
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-12 18:43:34 +0100

    Creacion de fichero environment

--- src/app/auth-service/auth.ts
+++ src/app/auth-service/auth.ts
@@ -2,17 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthInterface } from '../core/interfaces/auth-interface';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
 
-  private apiUrl = 'http://localhost:5000/api/auth';
+  private urlBase = environment.apiUrl;
   readonly #http = inject(HttpClient)
 
   login(nameuser: string, password: string): Observable<AuthInterface.InfoUser> {
-    return this.#http.post<AuthInterface.InfoUser>(`${this.apiUrl}/login`, { nameuser, password });
+    return this.#http.post<AuthInterface.InfoUser>(`${this.urlBase}/login`, { nameuser, password });
   }
 
   logout() {
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl: 'https://api.miapp.com/api/auth',  // URL real del backend en producción
+  production: true
+};
